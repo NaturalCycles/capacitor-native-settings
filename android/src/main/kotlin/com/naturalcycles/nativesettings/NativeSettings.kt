2d1f543dd5235c4bfedf22602ccb92dedb7b3a0f
jurajorszag <juraj.orszag@naturalcycles.com>
2021-03-15 11:22:33 +0100

    feat(android): add openInAppSettings event

--- android/src/main/kotlin/com/naturalcycles/nativesettings/NativeSettings.kt
+++ android/src/main/kotlin/com/naturalcycles/nativesettings/NativeSettings.kt
@@ -3,32 +3,33 @@ package com.naturalcycles.nativesettings
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
-import com.getcapacitor.NativePlugin
-import com.getcapacitor.Plugin
-import com.getcapacitor.PluginCall
-import com.getcapacitor.PluginMethod
+import com.getcapacitor.*
 
 @NativePlugin
 class NativeSettings : Plugin() {
-    @PluginMethod
-    fun open(call: PluginCall) {
-        val packageUri = Uri.parse("package:" + context.packageName)
-        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageUri)
-        context.startActivity(intent)
-        call.success()
-    }
+  @PluginMethod
+  fun open(call: PluginCall) {
+    val packageUri = Uri.parse("package:" + context.packageName)
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageUri)
+    context.startActivity(intent)
+    call.success()
+  }
 
-    @PluginMethod
-    fun openNotificationSettings(call: PluginCall) {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
-                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    .putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
+  @PluginMethod
+  fun openNotificationSettings(call: PluginCall) {
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+      val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+              .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+              .putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
 
-            context.startActivity(intent)
-            call.success()
-        } else {
-            open(call)
-        }
+      context.startActivity(intent)
+      call.success()
+    } else {
+      open(call)
     }
+  }
+
+  fun openInAppSettings() {
+    notifyListeners("openInAppSettings", JSObject(), true)
+  }
 }
